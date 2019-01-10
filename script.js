81ea9840d04d394c92f73728032c489e6296c8f7
dreyhiflden <dreyhiflden@gmail.com>
2019-01-10 18:02:53 +0200

    Add refactored worked prototype and final version of 1.1 task

--- script.js
+++ script.js
@@ -1,110 +1,48 @@
-// Task 1.1 Sum of odd numbers
+// JS Task 1.1 Sum of odd numbers
+
+// Refactored working prototype (except wee need set bigger arr_length to pass all tests)
+// Comments need to show way of thinking to solve this task
+// Correct version started at line 41
+
+const arr_length = 100; // Set length of our arrays
+
+/*
+  Here I create array, fill this array by method .fill()
+  and change values to index of each item by .map
+  filter array for odd number by .filter method.
+ */
+
+let oddNumbers = Array(arr_length) // create array to collect all odd numbers
+    .fill() // set all array items to empty value
+    .map((v, i) => i) // change each item value to item index
+    .filter(v => v % 2), // filter massive to odd numbers only
+
+  triangleNumbs = Array(arr_length) // create array to collect triangular number sequence
+    .fill() // set all array items to empty value
+    .map((v, i) => (i * (i + 1)) / 2), // change each item value to triangular number by their index
+
+  sortedOddNumbers = Array(100) // create array to hold sorted odd numbers like triangle (array of arrays)
+    .fill() // set all array items to empty value
+    .map((v, i) => oddNumbers.slice(triangleNumbs[i], triangleNumbs[i + 1]));
+    /*
+      change array items to result of slice oddNumbers array, start and end point of slice is values of triangleNumbs
+     */
 
 
-let oddNumbers = []; // Array to collect odd numbers
-let tria = [];       // Array to collect triangular numbers
-let sliced = [];     // Array of triangular numbers to make pyramid like figure
-let maxnumb;
+let rowSumOddNumbs = n => {
+  return sortedOddNumbers[--n].reduce((s, v) => s + v);
+};
 
+// console.table(sortedOddNumbers);
+// console.log(oddNumbers);
+// console.log(triangleNumbs);
 
-for (let i = 0; i < 1000; i++) { // Генерируем числа от и до
-  if (i % 2) { // проверячем odd число
-    oddNumbers.push(i); // если odd то добавляем в массив
-  }
+console.log(rowSumOddNumbs(3));
 
-  function triangleNumber(n)
-  {
-    return (n * (n + 1)) / 2;
-  }
+// Correct working version of 1.1 Sum of odd numbers
 
-  tria.push(triangleNumber(i));
+let rowSumOddNumbers = n => {
+  return n**3;
+};
 
-}
-
-function slicy(curr, next) {
-  sliced.push(oddNumbers.slice(curr, next));
-}
-
-for (let j = 0; j < oddNumbers.length / 6; j++) {
-  slicy(tria[j], tria[j+1]);
-}
-
-let num = sliced[1].reduce((sum, value) => sum + value);
-
-// for (let i = 0; i < numbers.length; i++) {
-//   function triangleNumber(n)
-//   {
-//     return (n * (n + 1)) / 2;
-//   }
-
-// x = i(i+1)/2;
-//   sliced.push(numbers.slice(triangleNumber(i), triangleNumber(++i)));
-
-  // let curr = triangleNumber(i);
-  // let next = triangleNumber(++i);
-  // let next = triangleNumber(++i);
-
-  // sliced.push(numbers.slice(0, 1));
-  // console.log(curr);
-// }
-//
-// function sliceee(curr, next) {
-//   sliced.push(numbers.slice(triangleNumber(i), triangleNumber(++i)));
-// }
-//
-// console.log(sliceee());
-
-// for (let i = 0; i < numbers.length; i++) {
-//     sliced.push(numbers.slice(i));
-// }
-// for (let j = 1; j < 10; j++) {
-//     sliced.push(numbers.slice(0, j));
-// }
-
-// То что должен в идеале получить
-// sliced.push(numbers.slice(0, 1)); // разница между числами 1
-// sliced.push(numbers.slice(1, 3)); // разница между числами 2
-// sliced.push(numbers.slice(3, 6)); // разница между числами 3
-// sliced.push(numbers.slice(6, 10)); // разница между числами 4 (берется предыдущее в начало и добавляется 4)
-// sliced.push(numbers.slice(10, 15)); // разница между числами 5 (берется предыдущее в начало и добавляется 4)
-
-
-//
-// let odds = numbers.filter(x => x % 2); // фильтруем
-//
-//
-// let tan
-//
-// function row(n) {
-//
-//     for (let i = 0; i < odds.length; i++) {
-//         for (let x = 1; x < n; x++) {
-//             ne
-//         }
-//     }
-//
-//
-//     // for (let i = 0; i <
-//     //   ; i++) {
-//     //
-//     // }
-//
-//
-//     return odds.slice(n + 1, n + 5);
-// }
-
-// let sum = ee.reduce((sum, value) => sum + value);
-
-
-// console.table(sliced);
-// console.log(tria);
-console.log(num);
-
-// function triangleNumber(n)
-// {
-//   return (n * (n + 1)) / 2;
-// }
-//
-// console.log(triangleNumber(3));
-// console.table(odds);
-// console.log(row(1));
+console.log(rowSumOddNumbers(3));
